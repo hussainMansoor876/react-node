cde473f2192de5938ba39764cafbaa05847a678a
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-09-30 01:07:05 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import moment from 'moment'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      epoch: '',
+      counter: null,
+      currentTime: 0,
+      metrics: '',
+      timeLoading: true,
+      metricsLoading: true
+    }
+  }
+
+  componentDidMount() {
+    this.getTime()
+    this.getMetrics()
+  }
+
+  epochFormat = (time) => moment(time).format('HH:mm:ss')
+
+  getTime = () => {
+    this.setState({ timeLoading: true, metricsLoading: true })
+    const { counter } = this.state
+    if (typeof (counter) === 'number') {
+      clearInterval(counter)
+    }
+    axios.get('/time', { headers: { mysecrettoken: 'Basic dXNlcjpwYXNzd29yZA==' } })
+      .then((res) => {
+        const { data } = res
+        var currentTime = Date.now() - data.properties.epoch.description - 18000000
+        this.setState({ epoch: this.epochFormat(currentTime), currentTime, timeLoading: false }, () => {
+          this.startCount()
+        })
+      })
+      .catch(() => {
+        this.setState({ timeLoading: false })
+      })
+  }
+
+  getMetrics = () => {
+    axios.get('/metrics', { headers: { mysecrettoken: 'Basic dXNlcjpwYXNzd29yZA==' } })
+      .then((res) => {
+        const { data } = res
+        this.setState({ metrics: data, metricsLoading: false })
+      })
+      .catch(() => {
+        this.setState({ metricsLoading: false })
+      })
+  }
+
+  startCount = () => {
+    var currentTime = this.state.currentTime
+    var counter = setInterval(() => {
+      this.setState({ epoch: this.epochFormat(currentTime + 1000) })
+      currentTime += 1000
+    }, 1000)
+    this.setState({ counter })
+    setTimeout(() => {
+      this.getTime()
+    }, 30000)
+  }
+
+  render() {
+    const { epoch, metrics, timeLoading, metricsLoading } = this.state
+    return (
+      <div>
+        {timeLoading && metricsLoading ? <div style={{ position: 'fixed', top: window.innerHeight / 2.5, left: window.innerWidth / 2.08 }}>
+          <div className="lds-ellipsis">
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+          </div>
+        </div> :
+          <div className="main-div">
+            <div className="left-div">
+              {epoch}
+            </div>
+            <div className="right-div">
+              {metrics}
+            </div>
+          </div>}
+      </div>
+    );
+  }
 }
 
 export default App;
